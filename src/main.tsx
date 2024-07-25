7c11ed7616b8781ee7ff362debb10b3d85455cee
AustinNick <46616395+AustinNick@users.noreply.github.com>
2024-07-25 19:18:10 +0700

    commnet strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  // Comment strict mode agar tidak double fetching API dan membuat error 429 Too many requests
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
