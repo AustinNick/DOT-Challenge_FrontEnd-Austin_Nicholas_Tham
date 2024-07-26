cf894dd6afeceee5ab51f4a70681b1dca2a27a5b
AustinNick <46616395+AustinNick@users.noreply.github.com>
2024-07-26 11:44:23 +0700

    add user

--- src/constants/userData.ts
+++ src/constants/userData.ts
@@ -1,15 +1,20 @@
-import { user } from "../types/user"
+import { User } from "../types/user"
 
-const userData: user[] = [
+const userData: User[] = [
     {
         id: 1,
-        name: 'Austin Nicholas Tham',
-        username: 'austinnick',
-        email: 'austinnicho81@gmail.com',
-        password: 'passwords'
+        name: "Austin Nicholas Tham",
+        username: "austinnick",
+        email: "austinnicho81@gmail.com",
+        password: "passwords"
+    },
+    {
+        id: 2,
+        name: "User 2",
+        username: "user",
+        email: "user@gmail.com",
+        password: "user"
     }
 ]
 
-export {
-    userData
-}
+export { userData }
